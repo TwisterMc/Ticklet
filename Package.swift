348aaaed31b579912dadab54556af5b6ac5472a3
Thomas McMahon <github@twistermc.net>
2026-01-04 18:57:54 -0600

    feat: enhance build process with optimization flags and versioning for packaging

--- Package.swift
+++ Package.swift
@@ -11,7 +11,10 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "Ticklet",
-            path: "Sources/Ticklet"
+            path: "Sources/Ticklet",
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+            ]
         ),
         .testTarget(
             name: "TickletTests",
